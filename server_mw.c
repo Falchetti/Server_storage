0a78bf1878988fd1fb3b27c3cc9daa2689372cc8
gfluon <giuliafalchetti00@gmail.com>
2022-08-03 03:12:24 +0200

    sketch server master-worker

--- server_mw.c
+++ server_mw.c
@@ -6,10 +6,11 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <time.h>
+#include <pthread.h>
 #include "icl_hash.h"
 
 #define DEBUG
-//#undef DEBUG
+#undef DEBUG
 
 #define UNIX_PATH_MAX 108 //lunghezza massima path
 #define MSG_SIZE  130 //path + spazio comandi 
@@ -20,7 +21,9 @@
 #define NBUCKETS  256 // n. di buckets nella tabella hash, forse sempre legato alla config 
 #define MAX_REQ   20 //n. max di richieste in coda 
 #define N 30
+#define THREAD_NUM 4
 
+//ricordati di pensare alle lock su l'hashtable 
 
 typedef struct open_node{
     int fd;
@@ -33,7 +36,6 @@ typedef struct file_info{
 	int lst_op; //controllo per la writeFile
 	char *cnt;
 } file_info;
-
 
 
 void print_deb(icl_hash_t *ht);
@@ -58,30 +60,228 @@ int removeF(icl_hash_t *ht, char *path, int fd);
 int readAF(icl_hash_t *ht, int fd, int seed);
 int readAllF(icl_hash_t *ht, int fd);
 
-icl_hash_t *hash_table;
+typedef struct elm_coda{
+	int fd;
+	struct elm_coda *next;
+} elm_coda;
 
+elm_coda *coda = NULL;
+pthread_mutex_t mutexCoda;
+pthread_cond_t condCoda;
 
+icl_hash_t *hash_table = NULL;
 
-int main(int argc, char *argv[]){
+void enqueue(int x){
+	elm_coda *new = malloc(sizeof(elm_coda));
+	elm_coda *aux = coda;
 	
-	int  fd_skt, fd_c; //socket passivo che accetta, socket per la comunicazione
-	char *msg; 
-	char *token, *token2;
-	char *tmpstr;
-	int seed = time(NULL); 
-	
-	struct sockaddr_un sa;
-	
-	memset(&sa, 0, sizeof(struct sockaddr_un));
-	strncpy(sa.sun_path, SOCKNAME, UNIX_PATH_MAX);
-	sa.sun_family = AF_UNIX;
-	
-	icl_hash_t *hash_table;
-	
-	if((hash_table = icl_hash_create(NBUCKETS, hash_pjw, string_compare)) == NULL){
-		fprintf(stderr, "Errore nella creazione della hash table\n");
-		return -1;
+	new->fd = x;
+	new->next = NULL;
+	if(coda == NULL){
+		coda = new;
 	}
+	else{
+		while(aux->next != NULL)
+			aux = aux->next;
+		aux->next = new;
+	}
+	return;
+}
+
+int dequeue(){ 
+	int x;
+	elm_coda *aux;
+	
+	if(coda == NULL)
+		return -1;
+	
+	aux = coda;
+	x = coda->fd;
+	coda = coda->next;
+	free(aux);
+	
+	return x;
+}
+
+
+int executeTask(int fd_c){ //qui faccio la read, capisco cosa devo fare, lo faccio chiamando la giusta funzione, rispondo al client, scrivo fd sulla pipe per il server  
+	/*int res;
+	res= fd * 2;
+	fprintf(stderr, "il doppio di %d è %d\n", fd, res);*/
+	
+	char *msg = malloc(MSG_SIZE*sizeof(char)); //controlla la malloc 
+	char *tmpstr, *token, *token2;
+	int seed = time(NULL); 
+		
+	if(read(fd_c, msg, MSG_SIZE) == -1){
+		perror("read socket lato server");
+		return -1; //ok?
+	}
+	
+	#ifdef DEBUG
+		fprintf(stderr, "\n*********Contenuto canale di comunicazione: %s*******\n\n", msg);
+	#endif
+	
+	token = strtok_r(msg, ";", &tmpstr);
+	
+	if(token != NULL){
+		
+		if(strcmp(token, "openfc") == 0){
+			token = strtok_r(NULL, ";", &tmpstr);
+			if(openFC(hash_table, token, fd_c) == -1){
+				return -1;
+			}
+		}
+		else if(strcmp(token, "openfl") == 0){
+			token = strtok_r(NULL, ";", &tmpstr);
+			if(openFL(hash_table, token, fd_c) == -1){
+				//i = N;
+			}
+		}
+		else if(strcmp(token, "openfo") == 0){
+			token = strtok_r(NULL, ";", &tmpstr);
+			if(openFO(hash_table, token, fd_c) == -1){
+				return -1;
+			}
+		}
+		else if(strcmp(token, "openfcl") == 0){
+			token = strtok_r(NULL, ";", &tmpstr);
+			if(openFCL(hash_table, token, fd_c) == -1){
+				return -1;
+			}
+		}
+		else if(strcmp(token, "disconnesso") == 0){//da chiudere i file e aggiornarli
+			close(fd_c); //non so se lo fa in automatico
+			return -1;
+		}
+		else if(strcmp(token, "closef") == 0){
+			token = strtok_r(NULL, ";", &tmpstr);
+			if(closeF(hash_table, token, fd_c) == -1){
+				return -1;
+			}
+		}
+		else if(strcmp(token, "readf") == 0){
+			token = strtok_r(NULL, ";", &tmpstr);
+			if(readF(hash_table, token, fd_c) == -1){
+				return -1;
+			}
+		}
+		else if(strcmp(token, "writef") == 0){
+			token = strtok_r(NULL, ";", &tmpstr);
+			token2 = strtok_r(NULL, ";", &tmpstr);
+			if(strcmp(token2, "$") == 0)
+				token2 = NULL;
+				
+			if(writeF(hash_table, token, token2, fd_c) == -1){
+				return -1;
+			}			
+		}
+		else if(strcmp(token, "lockf") == 0){
+			token = strtok_r(NULL, ";", &tmpstr);
+			if(lockF(hash_table, token, fd_c) == -1){
+				return -1;
+			}
+		}
+		else if(strcmp(token, "unlockf") == 0){
+			token = strtok_r(NULL, ";", &tmpstr);
+			if(unlockF(hash_table, token, fd_c) == -1){
+				return -1;
+			}
+		}
+		else if(strcmp(token, "appendTof") == 0){
+			token = strtok_r(NULL, ";", &tmpstr);
+			token2 = strtok_r(NULL, ";", &tmpstr);
+			if(strcmp(token2, "$") == 0)
+				token2 = NULL;
+				
+			if(appendToF(hash_table, token, token2, fd_c) == -1){
+				return -1;
+			}			
+		}
+		else if(strcmp(token, "removef") == 0){
+			token = strtok_r(NULL, ";", &tmpstr);
+			if(removeF(hash_table, token, fd_c) == -1){
+				return -1;
+			}
+		}
+		else if(strcmp(token, "readAf") == 0){
+			if(readAF(hash_table, fd_c, seed) == -1){
+				return -1;
+			}
+			seed++;
+		}
+		else if(strcmp(token, "readAllf") == 0){
+			if(readAllF(hash_table, fd_c) == -1){
+				return -1;
+			}
+		}	
+		else{
+			fprintf(stderr, "Opzione non ancora implementata\n");
+			write(fd_c, "Ok", 3);
+		}
+		
+		//free(msg); non ho capito chi fa la free di msg, mi sa la close, ma va bene?	
+	}
+
+	#ifdef DEBUG
+		fprintf(stderr,"\n---HASHTABLE:\n");
+		print_deb(hash_table);
+	#endif	
+	
+	//SCRIVI SULLA PIPE
+	
+	return 0;
+}
+
+void *init_thread(void *args){ //CONSUMATORE
+	int fd;
+	
+	while(1){
+		
+		pthread_mutex_lock(&mutexCoda); //sostituiscilo con un wrapper che fa gestione errori 
+		
+		while(coda == NULL) 
+			pthread_cond_wait(&condCoda, &mutexCoda); 
+		
+		if((fd = dequeue()) == -1){
+			fprintf(stderr,"Errore dequeue, init thread\n");
+			exit(1); //da modificare 
+		}
+
+		pthread_mutex_unlock(&mutexCoda);
+
+		if(executeTask(fd) == -1)
+			return -1; //In questo caso è ok? controlla pthread_create 
+	}
+	return (void *) 0; 
+}
+
+
+void *submitTask(int descr){ //PRODUTTORE
+	
+    pthread_mutex_lock(&mutexCoda);
+	
+	enqueue(descr);
+	
+	pthread_cond_signal(&condCoda); 
+	
+	pthread_mutex_unlock(&mutexCoda);
+	
+	return (void *) 0;
+}
+
+static void run_server(struct sockaddr *sa){
+	
+	int fd_skt, fd_c; 
+	int fd_num = 0; //si ricorda il max fd attivo 
+    int	fd; //indice per select
+
+	//char buf[N]; 
+	
+	fd_set set; //fd attivi
+	fd_set rdset; //fd attesi in lettura 
+	
+	//int nread; //num caratteri letti
 	
 	if((fd_skt = socket(AF_UNIX, SOCK_STREAM, 0)) == -1){
 		perror("Errore nella socket");
@@ -95,136 +295,98 @@ int main(int argc, char *argv[]){
 
 	listen(fd_skt, MAX_REQ); //fai gestire errore
 	
-	fd_c = accept(fd_skt, NULL, 0); //fai gestire errore, crea un nuovo socket per la comunicazione e ne restituisce il file descriptor
+	if(fd_skt < fd_num) //mi voglio ricordare l'indice max tra i descrittori attivi
+		fd_num = fd_skt;
 	
-    for(int i = 0; i < N ; i++){
-		msg = malloc(MSG_SIZE*sizeof(char)); //controlla la malloc 
+	FD_ZERO(&set); //per evitare errori prima lo azzero
+	FD_SET(fd_skt, &set); //metto a 1 il bit di fd_skt, fd_skt pronto
+	
+	for(int i = 0; i < N; i++){ //qui ci va un while(1)
 		
-		if(read(fd_c, msg, MSG_SIZE) == -1){
-			perror("read socket lato server");
-			return -1; //ok?
-	    }
+		rdset = set; //preparo la maschera per la select, ne servono due perchè select modifica quello che gli passi
 		
-		#ifdef DEBUG
-			fprintf(stderr, "\n*********Contenuto canale di comunicazione: %s*******\n\n", msg);
-		#endif
-		
-		token = strtok_r(msg, ";", &tmpstr);
-		
-		if(token != NULL){
-			
-			if(strcmp(token, "openfc") == 0){
-				token = strtok_r(NULL, ";", &tmpstr);
-				if(openFC(hash_table, token, fd_c) == -1){
-					i = N;
-				}
-			}
-			else if(strcmp(token, "openfl") == 0){
-				token = strtok_r(NULL, ";", &tmpstr);
-				if(openFL(hash_table, token, fd_c) == -1){
-					//i = N;
-				}
-			}
-			else if(strcmp(token, "openfo") == 0){
-				token = strtok_r(NULL, ";", &tmpstr);
-				if(openFO(hash_table, token, fd_c) == -1){
-					i = N;
-				}
-			}
-			else if(strcmp(token, "openfcl") == 0){
-				token = strtok_r(NULL, ";", &tmpstr);
-				if(openFCL(hash_table, token, fd_c) == -1){
-					i = N;
-				}
-			}
-			else if(strcmp(token, "disconnesso") == 0){//da chiudere i file e aggiornarli
-				close(fd_c); //non so se lo fa in automatico
-				i = N;
-			}
-			else if(strcmp(token, "closef") == 0){
-				token = strtok_r(NULL, ";", &tmpstr);
-				if(closeF(hash_table, token, fd_c) == -1){
-					i = N;
-				}
-			}
-			else if(strcmp(token, "readf") == 0){
-				token = strtok_r(NULL, ";", &tmpstr);
-				if(readF(hash_table, token, fd_c) == -1){
-					i = N;
-				}
-			}
-			else if(strcmp(token, "writef") == 0){
-				token = strtok_r(NULL, ";", &tmpstr);
-				token2 = strtok_r(NULL, ";", &tmpstr);
-				if(strcmp(token2, "$") == 0)
-					token2 = NULL;
-					
-				if(writeF(hash_table, token, token2, fd_c) == -1){
-					i = N;
-				}			
-			}
-			else if(strcmp(token, "lockf") == 0){
-				token = strtok_r(NULL, ";", &tmpstr);
-				if(lockF(hash_table, token, fd_c) == -1){
-					i = N;
-				}
-			}
-			else if(strcmp(token, "unlockf") == 0){
-				token = strtok_r(NULL, ";", &tmpstr);
-				if(unlockF(hash_table, token, fd_c) == -1){
-					i = N;
-				}
-			}
-			else if(strcmp(token, "appendTof") == 0){
-				token = strtok_r(NULL, ";", &tmpstr);
-				token2 = strtok_r(NULL, ";", &tmpstr);
-				if(strcmp(token2, "$") == 0)
-					token2 = NULL;
-					
-				if(appendToF(hash_table, token, token2, fd_c) == -1){
-					i = N;
-				}			
-			}
-			else if(strcmp(token, "removef") == 0){
-				token = strtok_r(NULL, ";", &tmpstr);
-				if(removeF(hash_table, token, fd_c) == -1){
-					i = N;
-				}
-			}
-			else if(strcmp(token, "readAf") == 0){
-				if(readAF(hash_table, fd_c, seed) == -1){
-					i = N;
-				}
-				seed++;
-			}
-			else if(strcmp(token, "readAllf") == 0){
-				if(readAllF(hash_table, fd_c) == -1){
-					i = N;
-				}
-			}	
-			else{
-				fprintf(stderr, "Opzione non ancora implementata\n");
-				write(fd_c, "Ok", 3);
-			}
-			
-			//free(msg); non ho capito chi fa la free di msg, mi sa la close, ma va bene?	
+		if(select(fd_num+1,	&rdset, NULL, NULL, NULL) == -1){ //si blocca finchè uno tra i descrittori in rdset non si blocca più su una read
+			perror("select");
+			return -1;
 		}
+		for(fd = 0; fd <= fd_num; fd++){ //guardo tutti i descrittori che select ha modificato in rdset
+			if(FD_ISSET(fd, &rdset)){ //controllo quali di questi è pronto in lettura
+				if(fd == fd_skt){ //se è il listen socket faccio accept 
+					fd_c = accept(fd_skt, NULL, 0);
+					FD_SET(fd_c, &set); //fd_c pronto
+					if(fd_c < fd_num) 
+						fd_num = fd_c;
+				}
+				else{ //altrimenti vado subito con la read
+					FD_CLR(fd, &set); //tolgo il fd da set 
+					submitTask(fd);
+					
+					/*nread = read(fd, buf, N);
+					if(nread == 0){ //client finito
+						FD_CLR(fd, &set); //tolgo il fd da set 
+						fd_num = aggiorna(&set);
+						close(fd);
+					}
+					else{
+						fprintf(err, "Server got: %s\n", buf);
+						write(fd, "Bye", 4);
+					}*/
+					
+					
+					//MANCA LA PARTE DELLA PIPE 
+				}
+			}
+		}
+	}
+}
 	
-		#ifdef DEBUG
-			fprintf(stderr,"\n---HASHTABLE:\n");
-			print_deb(hash_table);
-		#endif	
-	}		
-	
-	//close(fd_c);
-	close(fd_skt);
 
+
+int main(){
+	
+	pthread_t th[THREAD_NUM];
+	
+	pthread_mutex_init(&mutexCoda, NULL);
+	pthread_cond_init(&condCoda, NULL);
+	
+	struct sockaddr_un sa;
+	
+	memset(&sa, 0, sizeof(struct sockaddr_un));
+	strncpy(sa.sun_path, SOCKNAME, UNIX_PATH_MAX);
+	sa.sun_family = AF_UNIX;
+	
+	if((hash_table = icl_hash_create(NBUCKETS, hash_pjw, string_compare)) == NULL){
+		fprintf(stderr, "Errore nella creazione della hash table\n");
+		return -1;
+	}
+	
+	for(int i = 0; i < THREAD_NUM; i++){ //inizializzo pool di thread
+		if(pthread_create(&th[i], NULL, &init_thread, NULL) != 0){ //creo il thread che inizia ad eseguire init_thread
+			perror("Err in creazione thread");
+			return -1;
+		}
+	}
+	
+	run_server(&sa); 
+	
+	/*for(int i = 0; i < 10; i++){ //PRODUZIONE TASK, ci andrà la select poi 
+		//elm_coda t = { .x = 5 }; //produzione task 
+		int fd = i;
+		submitTask(fd); //sorta di enqueue 
+	}*/
+	
+	for(int i = 0; i < THREAD_NUM; i++){ //quando terminano i thread? Quando faccio la exit? 
+		if(pthread_join(th[i], NULL) != 0){ //aspetto terminino tutti i thread (la join mi permette di liberare la memoria occupata dallo stack privato del thread)
+			perror("Err in join thread");
+			return -1;
+		}
+	}
+	
+	pthread_mutex_destroy(&mutexCoda); //non so se vanno bene
+	pthread_cond_destroy(&condCoda);
+	
 	return 0;
 }
-
-//NB: quando chiudo la connessione devo modificare il file (togliere quel client dalla lista)
-// quando faccio qualsiasi op sul file (tranne quella speciale) devo resettare il flag
-//all'inizio forse dovrei inizializzare il tutto, non so se lo sto facendo o no 
 
 file_info *init_file(int lock_owner, int fd, int lst_op, char *contenuto){
 	file_info *tmp = malloc(sizeof(file_info)); 
@@ -775,8 +937,4 @@ int readAllF(icl_hash_t *ht, int fd){
 	
 	return 0;			
 }
-
-
-
-
 
